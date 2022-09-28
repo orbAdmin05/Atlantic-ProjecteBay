be32bd65311a93c7131f07736260a486b05af179
orbAdmin05 <111529834+orbAdmin05@users.noreply.github.com>
2022-09-28 11:52:44 -0400

    Add files via upload

--- src/test/java/utilities/DriverFactory.java
+++ src/test/java/utilities/DriverFactory.java
@@ -3,6 +3,13 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
 
@@ -18,10 +25,32 @@ public class DriverFactory {
     // Thread local driver object for WebDriver
     ThreadLocal<WebDriver> driver = ThreadLocal.withInitial(
             () -> { //Using Lambda Expression
-                WebDriverManager.chromedriver().setup();;
-                  // Can be replaced with other browser drivers
-                return new ChromeDriver();
-                  }
+                String environment = System.getProperty("environment") == null ? "local" : System.getProperty("environment");
+                String browser = System.getProperty("browser") == null ? "chrome" : System.getProperty("browser");
+                URL gridUrl = null;
+
+                try {
+                    gridUrl = new URL(ReadConfigFiles.getPropertyValues("GridURL"));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                if (environment.equals("remote") && browser.equals("chrome")) {
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    return new RemoteWebDriver(gridUrl, chromeOptions);
+                } else if (environment.equals("remote") && browser.equals("firefox")) {
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    return new RemoteWebDriver(gridUrl, firefoxOptions);
+                } else if (environment.equals("remote") && browser.equals("edge")) {
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    return new RemoteWebDriver(gridUrl, edgeOptions);
+                } else {
+                    WebDriverManager.chromedriver().setup();
+                    ;
+                    // Can be replaced with other browser drivers
+                    return new ChromeDriver();
+                }
+            }
                 );
 
     // Call this method to get the driver object and launch the browser
